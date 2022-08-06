bddf1da8fc2c2169b297346a474630a60e873f9e
NadyaCodes <nadyacorscadden@gmail.com>
2022-08-06 01:01:05 -0400

    add data to page

---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MOCK_PROJECTS } from './MockProjects';
+
+function ProjectsPage() {
+  return(
+    <div>
+      <h1>Projects</h1>
+      <pre>
+      {JSON.stringify(MOCK_PROJECTS, null, ' ')}
+      </pre>
+    </div>
+
+  )
+}
+
+export default ProjectsPage;
